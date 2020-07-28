71a84113764c0e0811a23dd5577769c258def3a4
Deivid Viana <53820145+DeividViana@users.noreply.github.com>
2020-07-28 18:15:23 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,10 +3,15 @@ const cards = document.querySelectorAll('.card')
 
 for (let card of cards) {
     card.addEventListener ("click", function(){
+        const imageID = card.getAttribute("id");
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("img").src = `/bootcamp-launchbase-desafios-02-master/layouts/assets/${imageID}`;
     })
 }
 
+
 document.querySelector('.material-icons').addEventListener("click", function(){
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector("img").src = ""
 })
+
